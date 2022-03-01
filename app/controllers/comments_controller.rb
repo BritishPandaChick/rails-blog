5f30983b28ed233a5d6957f3f8b820fd85a88ce4
BritishPandaChick <sbartley1080@gmail.com>
2022-03-01 00:32:23 -0500

    Fix destroy button for comments. Add comment partial. Refactor posts_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,10 @@ class CommentsController < ApplicationController
 		redirect_to post_path(@post)
 	end 
 
-	def destroy 
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy 
+		redirect_to post_path(@post) 
 	end
 end
